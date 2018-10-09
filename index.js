2e4ce9503e9dcb0a0833090d1f208ce3a5c89160
Lionel Adams <adamslionel@hotmail.co.uk>
2018-10-09 16:20:32 +0100

    feature: Add routing and route handlers to handle requests

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // Store all config
 const config = {
@@ -19,6 +20,7 @@ const httpServer = http.createServer((req, res) => server(req, res));
 // Server Logic
 const server = (req, res) => {
   // Get url and parse it
+  // NB: pass true param to get the query object
   const parsedUrl = url.parse(req.url, true);
 
   // Get path from url
@@ -27,32 +29,88 @@ const server = (req, res) => {
   // e.g. `/hello/world/` => `hello/world`
   const trimmedPath = pathName.replace(/^\/+|\/+$/g, '');
 
-  console.log('pathName: ', pathName);
-  console.log('trimmedPath: ', trimmedPath);
-
-  // get http method
+  // Get http method:
   // e.g: `GET / PUT / DELETE` etc...
+  // and convert to lowercase
   const method = req.method.toLowerCase();
 
   // Get the query string object
   const queryStringObject = parsedUrl.query;
 
-  // Get headers a sobject
+  // Get headers as a object
   const headers = req.headers;
 
-  console.log('method', method);
-  console.log('queryStringObject', queryStringObject);
-  console.log('headers', headers);
-  // Send the response
-  // construct data object to send to the handler
-  const data = {
-    trimmedPath,
-    queryStringObject,
-    method,
-  };
+  // Get the payload if there is one,
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  // Log the request path
-  res.end('Hello World');
+  // Append data stream into buffer, through the decoder
+  req.on('data', data => (buffer += decoder.write(data)));
+
+  // When finished, work out how to handle the request
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Choose the handler for this request
+    // if none found default to `notFound` handler
+    const chosenHandler =
+      typeof router[trimmedPath] !== 'undefined'
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // Construct data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    };
+
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the called back statusCode from the handler or default to 200
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+
+      // Use the called back payload from the handler or default to `{}`
+      payload = typeof payload === 'object' ? payload : {};
+      // Convert payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log(
+        `Returned response - statusCode: ${statusCode}, payload: ${payloadString}`,
+      );
+    });
+  });
+};
+
+// Define a Routing handler
+const handlers = {};
+
+// ping handler - confirms server is running
+handlers.ping = (data, callback) => {
+  callback(200);
+};
+
+// hello handler - returns a welcome message
+handlers.hello = (data, callback) => {
+  callback(200, { message: `Welcome to my simple JSON API` });
+};
+
+// Not found handler:
+// - Used when no handler can be found in the router
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  ping: handlers.ping,
+  hello: handlers.hello,
 };
 
 // Start the server and listen on chosen port
